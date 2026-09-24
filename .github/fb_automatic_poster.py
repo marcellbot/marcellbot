6d8b0124e9978abfef5e9d8dae1ca90795be7d08
marcellbot <martavalentinyi@gmail.com>
2026-09-25 00:48:54 +0200

    Update API keys in fb_automatic_poster.py
    Replaced environment variable keys with hardcoded values for XAI API and Facebook user access token.

--- .github/fb_automatic_poster.py
+++ .github/fb_automatic_poster.py
@@ -16,8 +16,8 @@ import time
 
 load_dotenv()
 
-XAI_API_KEY = os.getenv("XAI_API_KEY")
-FACEBOOK_USER_ACCESS_TOKEN = os.getenv("FB_USER_TOKEN") # Ehhez kell Business jogosultság
+XAI_API_KEY = os.getenv("xai-IWAr5f5NCcZZq5b97Bvp2Tmv7emMlH18WZF7E2cXXmdQ3Wr3wak7XsDUtDzGqtWZqr9P7fP6zta29hTT")
+FACEBOOK_USER_ACCESS_TOKEN = os.getenv("EAAKLM4j9TloBR0KNl8M1SVawvuBbbUMw584G0HdteuPIsrhZCpshjUVUaHKCfM5sbA6PAP8wSZBssTRA8jbn0R4jZA1BEgsWZC9T5XO8MECBqZCdwTnHMhvKOIdAZCtY0rmuET6DqzX1bGcZCcmRnLxLMvDF0T6zfD0BhB6L9ZAZAPYl2ixa3YiR6SPJqNRBWz2ZC8b4sFDfJjTEbOz3GCHOwjVwZCoXMR8qCnY1QNpMAZDZD") # Ehhez kell Business jogosultság
 
 client = OpenAI(api_key=XAI_API_KEY, base_url="https://api.x.ai/v1")
 
